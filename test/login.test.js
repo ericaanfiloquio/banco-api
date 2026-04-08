9919b1875ed4c9fa768503b93629110b8c9d98ec
Erica Anfiloquio <ericafanfiloquio@gmail.com>
2026-04-08 13:34:44 -0300

    Starting project

---
+++ test/login.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const { expect } = require('chai')
+
+describe('Login', () => { // função anônima: não coloca nada no parênteses
+    describe('POST /login', () => {
+        it('Deve retornar 200 com um token quando usar credenciais validas', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json') //set é para colocar o header
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                 })
+            
+            console.log(resposta.body)
+            console.log(resposta.status)
+
+            expect(resposta.body.token).to.be.a('string');
+            expect(resposta.status).to.equal(200); //para ter acesso a um elemento dentro da resposta, usa essa sintax
+        })
+     })
+})
